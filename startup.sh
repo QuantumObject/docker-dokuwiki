ef49d7ce242804aa21eb3633b4609eda6aab289a
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2020-04-05 23:54:38 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
 set -e
+#in case Volume are empty
+if [ "$(ls -A /var/www/dokuwiki/conf)" ]; then
+    echo "conf folder with data"    
+else
+    cp -Rp /var/backup/conf/* /var/www/dokukiki/conf/ 
+    chown www-data:www-data /var/www/dokukiki/conf
+fi
+# for the other folder Volume
+if [ "$(ls -A /var/www/dokuwiki/data)" ]; then
+    echo "data folder with data"    
+else
+    cp -Rp /var/backup/data/* /var/www/dokukiki/data/ 
+    chown www-data:www-data /var/www/dokukiki/data
+fi
+
+
 
 if [ -f /etc/configured ]; then
         echo 'already configured'
