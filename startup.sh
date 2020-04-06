7bfddcb48def649607cd497a843cc058be6fd005
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2020-04-06 00:58:41 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -6,15 +6,15 @@ set -e
 if [ "$(ls -A /var/www/dokuwiki/conf)" ]; then
     echo "conf folder with data"    
 else
-    cp -Rp /var/backups/conf/* /var/www/dokukiki/conf/ 
-    chown www-data:www-data /var/www/dokukiki/conf
+    cp -Rp /var/backups/conf/* /var/www/dokuwiki/conf/ 
+    chown www-data:www-data /var/www/dokuwiki/conf
 fi
 # for the other folder Volume
 if [ "$(ls -A /var/www/dokuwiki/data)" ]; then
     echo "data folder with data"    
 else
-    cp -Rp /var/backups/data/* /var/www/dokukiki/data/ 
-    chown www-data:www-data /var/www/dokukiki/data
+    cp -Rp /var/backups/data/* /var/www/dokuwiki/data/ 
+    chown www-data:www-data /var/www/dokuwiki/data
 fi
 
 # if already configured continue
