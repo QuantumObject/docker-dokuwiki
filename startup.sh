06849bac62cb006b705c9a04c7bf00ca0010dfeb
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2020-04-08 10:21:57 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -22,7 +22,8 @@ if [ -f /etc/configured ]; then
         echo 'already configured'
 else
       #code that need to run only one time ...
-        
+        mv /var/www/dokukiki/.htaccess.dist /var/www/dokukiki/.htaccess
+        a2enmod rewrite
         #needed for fix problem with ubuntu and cron
         update-locale 
         date > /etc/configured
